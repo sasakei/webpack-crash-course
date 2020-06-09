bdd60c5c886de5d9a32beaf64997e6641128ea08
sasaki kei <sasakei1021@icloud.com>
2020-06-09 09:34:55 +0900

    babel-and-react

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const outputPath = path.resolve(__dirname, 'dist')
 
 module.exports = {
@@ -10,6 +10,11 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
             {
                 test: /\.css$/,
                 use: [
@@ -33,9 +38,19 @@ module.exports = {
                     name: './images/[name].[ext]'
                 }
             },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
         ]
     },
     devServer: {
         contentBase: outputPath
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
 }
